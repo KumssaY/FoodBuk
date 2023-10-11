551c07278551e00d2aad8c03ec3fe30357ce0dd0
KumssaY <nehemiahyoseph@gmail.com>
2023-10-11 12:00:24 +0300

    This is my second commit

--- JS/script.js
+++ JS/script.js
@@ -21,6 +21,11 @@ function goToComplaintsPage() {
         window.location.href = 'complaints.html';
 }
 
+function goToGallaryPage() {
+    // Navigate to the complaints page
+    window.location.href = 'gallary.html';
+}
+
 function redirectToHomePage() {
     // Navigate to the home page (replace 'home.html' with the actual URL of your home page)
     window.location.href = 'home.html';
