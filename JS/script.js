d1d47357115657d644f4d5fef6ea7b6adb4c2970
KumssaY <nehemiahyoseph@gmail.com>
2023-10-18 14:48:56 +0300

    3rd upload

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,3 @@
-function addFood() {
-    const availableFoods = document.getElementById('availableFoods');
-    const newFoodItem = document.createElement('li');
-    newFoodItem.innerHTML = `
-        <label>
-            <input type="checkbox" name="foodSelection" value="foodN">
-            New Food - $X
-            <button class="btn btn-sm btn-danger remove-food" onclick="removeFood(this)">Remove</button>
-        </label>
-    `;
-    availableFoods.appendChild(newFoodItem);
-}
-
-function removeFood(button) {
-    const foodItem = button.parentNode.parentNode;
-    foodItem.parentNode.removeChild(foodItem);
-}
-
-function goToComplaintsPage() {
-        // Navigate to the complaints page
-        window.location.href = 'complaints.html';
-}
-
 function goToGallaryPage() {
     // Navigate to the complaints page
     window.location.href = 'gallary.html';
